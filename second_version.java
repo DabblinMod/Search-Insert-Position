0b206b630a9b364692e9e98c1f199b5339514ab9
Samuel Kusi-Amportey <92940110+DabblinMod@users.noreply.github.com>
2023-04-25 21:51:14 -0400

    updated

--- second_version.java
+++ second_version.java
@@ -1,22 +1,26 @@
-public class YourClassNameHere {
+public class Solution {
+    public int binarySearch(array, target){
+        int mid=0;
+          int low=0, high=array.length-1;
+          while(low < high){
+            mid= (low + high) / 2;
+
+            if(target > array[mid])
+              low= mid + 1;
+
+            else if(target < array[mid])
+              high= mid-1;
+
+            else if(target==array[mid]){
+              return mid;
+            }
+        
+    }
     public static void main(String[] args) {
       int target=7;
       int[] array={2, 3, 5, 7, 9};
-      int mid=0;
-      int low=0, high=array.length-1;
-      while(low < high){
-        mid= (low + high) / 2;
-        
-        if(target > array[mid])
-          low= mid + 1;
-          
-        else if(target < array[mid])
-          high= mid-1;
-          
-        else if(target==array[mid]){
-          System.out.print("Mid: "+mid);
-          break;
-        }
+      Solution s=new Solution();
+        assertEquals(s.binarySearch(array, target), 3);
       }
     }
 }
